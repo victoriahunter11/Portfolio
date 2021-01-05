f95cb1ab31b23ea6117e1ba4a1492f23460410d9
Victoria Hunter <victoria.jane09@gmail.com>
2021-01-05 12:05:15 -0500

    Styling for bootstrap

--- libs/bootstrap/js/bootstrap.bundle.js
+++ libs/bootstrap/js/bootstrap.bundle.js
@@ -2523,7 +2523,7 @@
    * @method
    * @memberof Popper.Utils
    * @argument {String} property (camelCase)
-   * @returns {String} prefixed property (camelCase or PascalCase, depending on the vendor prefix)
+   * @returns {String} prefixed property (camelCase or PascalCase, depending on the libs prefix)
    */
   function getSupportedPropertyName(property) {
     var prefixes = [false, 'ms', 'Webkit', 'Moz', 'O'];
